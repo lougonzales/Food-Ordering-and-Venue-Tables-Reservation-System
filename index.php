264a16f98d4f2789d2ce77034c483ad62360a24f
lougonzales <lougonzales1998@gmail.com>
2019-02-20 19:50:45 +0800

    Draft 1

--- index.php
+++ index.php
@@ -2,100 +2,129 @@
     <head>
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    </head>
-    <body>
-        <!-- Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+         <!-- Bootstrap JS -->
+         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <style>
+            .carousel {
+                border-radius: 10px 10px 10px 10px;
+                overflow: hidden;
+                ...
+            }
+        </style>
+    </head>
+    <body style="background-color: #D2D7D3">
         <!-- Nav Bar -->
-        <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Jessu's Cuisine</a>
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #CF3A24">
+            <a class="navbar-brand" style="color: white" href="#">Jessu's Cuisine</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">SIGN IN</a>
+                        <a class="nav-link" href="#" style="color: white">Login</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">CREATE ACCOUNT</a>
+                   <li class="nav-item">
+                       <button type="button" class="btn btn-primary">Create Account</button>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
             </div>
         </nav>
-        <!-- Carousel -->
-        <div class="container-fluid">
+        <!-- Content 1 -->
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-sm">
+                    <div class="jumbotrons" style="background-color: #D2D7D3">
+                        <h1 class="display-4">JESSU'S CUISINE</h1>
+                        <p class="lead">
+                            Celebrate your birthdays or any event with your 
+                            family and us or order food to pick up. 
+                        </p>
+                        <hr class="my-4">
+                        <p>Located at #000 abcdefgh, Teresa, Sta. Mesa, Manila</p>
+                        <p class="lead">
+                            <a class="btn btn-success btn-lg" href="#" role="button">Order Now</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-7 ml-auto">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="images/img1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="images/img2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="images/img3.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Content 2 -->
+        <div class="container-fluid mt-5" style="background-color: #CF3A24">
             <div class="row">
-                <div class="col-sm-8 offset-sm-2">
-                    <div class="bd-example">
-                        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="img1.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img2.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                <h5>Second slide label</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img src="img3.jpg" class="d-block w-100" alt="...">
-                                <div class="carousel-caption d-none d-md-block">
-                                <h5>Third slide label</h5>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                </div>
-                            </div>
-                            </div>
-                            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                            </a>
+                <div class="col-sm p-3 text-center">
+                    <div class="card" style="width: 18rem; background-color: #CF3A24; border: none">
+                        <img src="img4.png" class="card-img-top mx-auto" alt="..." style="width: 10rem; position: center">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color: white">Burger</h5>    
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm p-3 text-center">
+                <div class="card" style="width: 18rem; background-color: #CF3A24; border: none">
+                        <img src="img5.png" class="card-img-top mx-auto" alt="..." style="width: 10rem;">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color: white">Chicken</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm p-3 text-center">
+                    <div class="card" style="width: 18rem; background-color: #CF3A24; border:none">
+                        <img src="img6.png" class="card-img-top mx-auto" alt="..." style="width: 10rem;">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color: white">Spaghetti</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm p-3 text-center">
+                    <div class="card" style="width: 18rem; background-color: #CF3A24; border: none">
+                        <img src="img7.png" class="card-img-top mx-auto" alt="..." style="width: 10rem;">
+                        <div class="card-body">
+                            <h5 class="card-title" style="color: white">Beverage</h5>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- Cards -->
-        <div class="row p-5">
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+            <div class="row p-4">
+                <a href="#" class="btn btn-dark ml-auto" style="color: white">View Menu</a>
             </div>
-            <div class="col-sm-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+        </div>
+        <!-- Content 3 -->
+        <div class="container p-5">
+            <div class="row text-center">
+                <div class="col-sm">
+                    <h1>Food Pick up</h1>
+                </div>
+                <div class="col-sm">
+                    <h1>Table Reservation</h1>
                 </div>
             </div>
         </div>
+        <!-- Content 4 -->
     </body>
-</html>
+</html> 
